341c3a5ac6dec7c634221b60465849eac04be892
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-06-18 14:11:35 -0700

    problems 1&2 done

--- solution1.py
+++ solution1.py
@@ -1,1 +1,13 @@
 # Write code for algorithm 1 below
+def count_down(n):
+    #base case
+    if n == 0:
+        return
+    #recursive case
+    else:
+        print(n)
+        count_down(n-1)
+
+#test
+n = 8
+count_down(n)
--- solution3.py
+++ solution3.py
@@ -1,1 +1,24 @@
 # Write code for algorithm 3 below
+def fib_seq(n):
+    if n == 0:
+        return 0
+    elif n <= 1:
+        return n
+    else:
+        return fib_seq(n-1) + fib_seq(n-2)
+
+for i in range(0,10):
+    print(fib_seq(i))
+
+
+#iterative-not done
+def fibseq2(n):
+    if n <= 1:
+        return n 
+    result = [0,1]
+    for i in range(2, n+1):
+        result.append(result[n-1] + result[n-2])
+        return result[n]
+
+for i in range(0, 10):
+    print(fibseq2(3))    
